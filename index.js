54a61e5d553b43a902ea33027b1478aa14b86bb3
nyzs <neazenmeh@gmail.com>
2021-06-24 17:34:24 +0200

    i'm stopping here for today

--- index.js
+++ index.js
@@ -14,10 +14,23 @@ app.use(
   })
 );
 
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/auth", require("./routes/authRoute"));
+app.use("/todo", require("./routes/todoRoute"));
+app.use("/kana", require("./routes/kanaRoute"));
+app.use("/users", require("./routes/usersRoute"));
+
 mongoose.connect(
   process.env.MONGO_DB_URI,
   {
@@ -32,8 +45,3 @@ mongoose.connect(
     );
   }
 );
-
-app.use("/auth", require("./routes/authRoute"));
-app.use("/todo", require("./routes/todoRoute"));
-app.use("/kana", require("./routes/kanaRoute"));
-app.use("/users", require("./routes/usersRoute"));
