4147459ee7d193571fe0758cee94c03f6cf53cb8
nyzs <neazenmeh@gmail.com>
2021-06-24 16:33:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,17 +9,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  "https://maku.netlify.app/",
-  "http://maku.netlify.app/",
-  "http://localhost:3000",
-];
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: ["https://maku.netlify.app/", "http://localhost:3000"],
     credentials: true,
   })
 );
